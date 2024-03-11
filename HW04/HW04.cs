dcea0894dae7f5cd8d35efcbf667e10be997ea48
ArtB <89324748589@mail.ru>
2024-03-11 17:03:49 +0500

    HW04

---
+++ HW04/HW04.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Homework04
+{
+    internal class HW
+    {
+        public static void FindSum(int[] arr, int target) // Метод для поиска трех чисел в массиве
+        {
+            for (int i = 0; i < arr.Length; i++) // Перебор всех комбинаций трёх чисел
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+
+
+                        if (arr[i] + arr[j] + arr[k] == target) // Если сумма трех чисел равна целевому числу, выводим на экран
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine($"{arr[i]}+{arr[j]}+{arr[k]}={target}");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+   
+ }
---
+++ HW04/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿using System;
+
+namespace Homework04
+{
+    internal class Program
+    {
+        static void Main(string[] args) // вход
+        {
+            int[] array = { 2, 4, 5, 6, 7, 8, 9, 10, 12, 15, 17 }; // массив
+            int target = 25; // задание целевого числа
+
+            HW.FindSum(array, target); // вызов метода FindSum из класса HW для поиска  3 чисел равным target
+
+
+
+        }
+    }
+}
